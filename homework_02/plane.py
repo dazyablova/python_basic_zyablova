7ca5a80fcb5e7241f34f850c292fe9e262106d82
Darya Zyablova <dazyablova@mts.ru>
2023-07-17 11:52:09 +0300

    Add update

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,5 +1,7 @@
 from homework_02 import exceptions
 from homework_02.base import Vehicle
+from homework_02.exceptions import CargoOverload
+
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
@@ -7,13 +9,16 @@ from homework_02.base import Vehicle
 
 class Plane(Vehicle):
     def __init__(self, brand, model, year, max_cargo):
-        super().__init__(brand, model, year)
+        super().__init__(weight=0, fuel=0, fuel_consumption=0)
+        self.brand = brand
+        self.model = model
+        self.year = year
         self.cargo = 0
         self.max_cargo = max_cargo
 
     def load_cargo(self, amount):
         if self.cargo + amount > self.max_cargo:
-            raise exceptions.CargoOverload("Cargo overload")
+            raise CargoOverload("Cargo overload")
         self.cargo += amount
 
     def remove_all_cargo(self):
