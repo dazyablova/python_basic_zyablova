9c4fb8ef4dfe65b30cd95c473590bbd5769558a2
Darya Zyablova <dazyablova@mts.ru>
2023-10-03 16:47:41 +0300

    Add homework 7

---
+++ homework_07/test_colories.py
@@ -0,0 +1,51 @@
+import datetime as dt
+
+
+class Calculator:
+
+    def __init__(self, limit):
+        self.limit = limit
+        self.records = []
+
+    def get_today_stats(self):
+        today = dt.date.today()
+        return sum(record.amount for record in self.records
+                   if record.date == today)
+
+    def add_record(self, record):
+        self.records.append(record)
+
+    def get_week_stats(self):
+        today = dt.date.today()
+        date_week_ago = today - dt.timedelta(days=7)
+        return sum(record.amount for record in self.records
+                   if date_week_ago < record.date <= today)
+
+    def get_remained(self):
+        remained = self.limit - self.get_today_stats()
+        return remained
+
+
+class Record:
+
+    def __init__(self, amount: float, comment: str, date=None):
+        self.amount = amount
+        self.comment = comment
+        if date is None:
+            self.date = dt.date.today()
+
+        else:
+            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+
+    def __str__(self):
+        return f'Record({self.amount}, {self.date}, {self.comment})'
+
+
+class CaloriesCalculator(Calculator):
+
+    def get_calories_remained(self):
+        g_rem = self.get_remained()
+        if g_rem <= 0:
+            return 'Превышение лимита калорий'
+        return ('Недостаточно калорий, рекомендуется добрать'
+                f'не более {g_rem} кКал')
